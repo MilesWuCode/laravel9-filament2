ba0473bfa88839da3562de3912329a2ddb0d5724
Miles Wu <slion0725@gmail.com>
2022-06-27 00:40:41 +0800

    upd

--- app/Filament/Resources/BannerResource.php
+++ app/Filament/Resources/BannerResource.php
@@ -12,6 +12,7 @@ use Filament\Resources\Table;
 use Filament\Tables;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Database\Eloquent\Collection;
 
 class BannerResource extends Resource
 {
@@ -36,6 +37,7 @@ class BannerResource extends Resource
     {
         return $table
             ->columns([
+                Tables\Columns\TextColumn::make('id'),
                 Tables\Columns\TextColumn::make('name'),
                 Tables\Columns\TextColumn::make('url'),
                 Tables\Columns\TextColumn::make('order_column'),
@@ -44,7 +46,6 @@ class BannerResource extends Resource
                 Tables\Columns\TextColumn::make('updated_at')
                     ->dateTime(),
             ])
-            ->defaultSort('order_column', 'asc')
             ->filters([
                 //
             ])
@@ -54,6 +55,66 @@ class BannerResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\BulkAction::make('sortable')
+                    ->icon('heroicon-o-switch-vertical')
+                    ->deselectRecordsAfterCompletion()
+                    ->action(function (Collection $records, array $data): void {
+                        if (count($records) === 1) {
+                            $record = $records->first();
+
+                            if ($record->id !== $data['to']) {
+                                $orderColumn = $record->determineOrderColumnName();
+
+                                $target = Banner::find($data['to']);
+
+                                $order = [$target->$orderColumn, $record->$orderColumn];
+
+                                sort($order);
+
+                                $startOrder = $order[0];
+
+                                $ids = Banner::whereBetween($orderColumn, $order)
+                                    ->whereNotIn('id', [$target->getKey(), $record->getKey()])
+                                    ->ordered()
+                                    ->pluck('id')
+                                    ->toArray();
+
+                                if($record->$orderColumn > $target->$orderColumn && $data['shift'] === 'up') {
+                                    // 7 -> 4+ -> 4,5,6,7 = 7,4,5,6
+                                    array_unshift($ids, $record->getKey(), $target->getKey());
+
+                                }else if($record->$orderColumn > $target->$orderColumn && $data['shift'] === 'down') {
+                                    // 7 -> 4- -> 4,5,6,7 = 4,7,5,6
+                                    array_unshift($ids, $target->getKey(), $record->getKey());
+
+                                }else if($record->$orderColumn < $target->$orderColumn && $data['shift'] === 'up') {
+                                    // 4 -> 7+ -> 4,5,6,7 = 5,6,4,7
+                                    array_push($ids, $record->getKey(), $target->getKey());
+
+                                }else if($record->$orderColumn < $target->$orderColumn && $data['shift'] === 'down') {
+                                    // 4 -> 7- -> 4,5,6,7 = 5,6,7,4
+                                    array_push($ids, $target->getKey(), $record->getKey());
+
+                                }
+
+                                Banner::setNewOrder($ids, $startOrder);
+                            }
+                        }
+                    })
+                    ->form([
+                        Forms\Components\Select::make('to')
+                            ->label('To')
+                            ->options(Banner::query()->ordered()->pluck('name', 'id'))
+                            ->required(),
+
+                        Forms\Components\Radio::make('shift')
+                            ->label('Shift')
+                            ->options([
+                                'up' => 'Up',
+                                'down' => 'Down',
+                            ])
+                            ->default('up')
+                    ])
             ]);
     }
 
@@ -72,5 +133,10 @@ class BannerResource extends Resource
             'view' => Pages\ViewBanner::route('/{record}'),
             'edit' => Pages\EditBanner::route('/{record}/edit'),
         ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return static::getModel()::query()->orderBy('order_column', 'asc');
     }
 }
