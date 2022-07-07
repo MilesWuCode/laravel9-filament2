48903e3d92d05cd166a0ff8458452761200354b2
Miles Wu <slion0725@gmail.com>
2022-07-07 17:21:16 +0800

    upd

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\EloquentSortable\Sortable;
 use Spatie\EloquentSortable\SortableTrait;
 
 class Banner extends Model implements Sortable
 {
+    use LogsActivity;
     use HasFactory;
     use SortableTrait;
 
@@ -24,4 +27,10 @@ class Banner extends Model implements Sortable
         'order_column_name' => 'order_column',
         'sort_when_creating' => true,
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logAll();
+    }
 }
